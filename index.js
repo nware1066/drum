ef6c46188e1419e5e1f788fc3b214980ac4e209c
Naomi Ware <nware1066@yahoo.com>
2021-04-20 22:15:59 -0600

    Add eventListener to play audio file on click

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let numberOfDrums = document.querySelectorAll(".drum").length;
 
-for ( var i = 0; i < numberOfDrums; i++) {
+for ( var i = 0; i<numberOfDrums; i++) {
 
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        let crash = new Audio("sounds/crash.mp3");
+        crash.play();
+    });
 }
-
